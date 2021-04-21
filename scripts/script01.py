04cd4b907165fcdc3cc6b23da89c860cf47ef48d
gonzalezmorales <gonzalezmorales@gmail.com>
2021-04-21 14:49:31 -0400

    add metadata.xlsx

--- scripts/script01.py
+++ scripts/script01.py
@@ -2,13 +2,17 @@ import utils
 import json
 
 # Read minset_indicators_catalog
-
 minset_indicators_catalog = utils.xlsx2dict('master_data/MINSET_Indicators.xlsx', 'CL_INDICATORS')
 
+# Read minset_series_catalog
 minset_series_catalog = utils.xlsx2dict('master_data/MINSET_series_catalog.xlsx', 'CL_SERIES')
 
-print(minset_indicators_catalog[0])
-print(minset_series_catalog[0])
+# Read metadata
+metadata_info = utils.xlsx2dict('master_data/Metadata.xlsx', 'Metadata')
+
+# print(minset_indicators_catalog[0])
+# print(minset_series_catalog[0])
+print(metadata_info[0])
 
 metadata = []
 
@@ -20,8 +24,11 @@ for i in minset_indicators_catalog:
     
     i['series'] = []
 
-
     for s in minset_series_catalog:
+        
+        # if s['MINSET_SERIES'] != "72":
+        #     continue
+
         series_detail = dict()
         if s['INDICATOR_ID'] == i['INDICATOR_ID']:
             series_detail['MINSET_SERIES'] = s['MINSET_SERIES']
@@ -29,11 +36,32 @@ for i in minset_indicators_catalog:
             series_detail['Retired'] = s['Retired']
             series_detail['SDG_SeriesID'] = s['SDG_SeriesID']
 
+            series_detail['Metadata'] = []
+
+            metadata_i_s = utils.select_dict(metadata_info, 
+                                            {'INDICATOR_ID': i['INDICATOR_ID'], 
+                                            'MINSET_SERIES': s['MINSET_SERIES']}
+                                            )
+
+
+            for m in metadata_i_s:
+                metadata_dict = {}
+                metadata_dict['METADATA_CATEGORY']= m['METADATA_CATEGORY']
+                metadata_dict['METADATA_CATEGORY_DESC']= m['METADATA_CATEGORY_DESC']
+                metadata_dict['METADATA_DESCRIPTION']= m['METADATA_DESCRIPTION']
+                metadata_dict['METADATA_SOURCE']= m['METADATA_SOURCE']
+                metadata_dict['SOURCE_YEAR']= m['SOURCE_YEAR']
+
+                series_detail['Metadata'].append(metadata_dict)
+                                    
+
+
             for k in series_detail.keys():
                 if series_detail[k] == 'nan':
                     series_detail[k] = None
 
-            i['series'].append(series_detail)
+            i['series'].append(series_detail)    
+
     metadata.append(i)
 
 
