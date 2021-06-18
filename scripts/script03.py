ff3e2903f9404f3d909b466bea467004a981bb44
gonzalezmorales <gonzalezmorales@gmail.com>
2021-06-18 17:24:44 -0400

    basic ts availability

--- scripts/script03.py
+++ scripts/script03.py
@@ -5,41 +5,46 @@ from os.path import isfile, join
 
 # Read csv file
 
-# f = utils.tsv2dictlist('series_data/ind_1__series_72.csv')
-
 datafiles = [f for f in listdir('series_data/') if isfile(join('series_data/', f))]
-
-
 
 all_columns_list = set()
 
-for df in datafiles:
-    f = utils.tsv2dictlist('series_data/'+df)
-    all_columns = set(f[0].keys())
+# for df in datafiles:
 
-    #---------------------------------
-    # Check that X,Y and SORUCE_DETAIL_URL are all uppercase
-    #---------------------------------
-    if ('x' in all_columns):
-        print (f"x Exists in {df}")
+#     if df !='ind_1__series_72.csv':
+#         continue
 
-    if ('y' in all_columns):
-        print (f"y Exists in {df}")
+#     f = utils.tsv2dictlist('series_data/'+df)
+#     all_columns = set(f[0].keys())
 
-    if ('SOURCE_DETAIL_url' in all_columns):
-        print (f"SOURCE_DETAIL_url Exists in {df}")
+#     #---------------------------------
+#     # Check that X,Y and SORUCE_DETAIL_URL are all uppercase
+#     #---------------------------------
+#     if ('x' in all_columns):
+#         print (f"x Exists in {df}")
 
-    all_columns_list.update(all_columns)
+#     if ('y' in all_columns):
+#         print (f"y Exists in {df}")
 
-all_columns_list = list(all_columns_list)
+#     if ('SOURCE_DETAIL_url' in all_columns):
+#         print (f"SOURCE_DETAIL_url Exists in {df}")
 
-all_columns_list.sort()
+#     all_columns_list.update(all_columns)
 
-print(f'\nList of all columns across all files:\n{all_columns_list}')
+# all_columns_list = list(all_columns_list)
+
+# all_columns_list.sort()
+
+# print(f'\nList of all columns across all files:\n{all_columns_list}')
 
 
-#----------- Iterate here ---------#
+# #----------- Iterate here ---------#
+
+availability = []
+
 for f in datafiles:
+    # if f !='ind_1__series_72.csv':
+    #     continue
 
     print(f"\nCurrent file: {f}")
 
@@ -71,6 +76,8 @@ for f in datafiles:
                     'UPPER_BOUND_MODIFIER',
                     'VALUE_CATEGORY',
                     'VALUE_CATEGORY_DESC',
+                    'REPORTING_TYPE', 
+                    'REPORTING_TYPE_DESC',
                     'X',
                     'Y']
 
@@ -78,27 +85,78 @@ for f in datafiles:
     
     unique_TSK_values = utils.unique_dicts(utils.subdict_list(f2, non_TSK_columns, exclude=True))
 
+    # print(unique_TSK_values[0])
 
+    utils.dictList2tsv(unique_TSK_values, 'test.txt')
 
     #------------------------
     # Test for unique records
     #------------------------
 
 
-    has_duplicates = []
+    # has_duplicates = []
+
+    # for ts in unique_TSK_values:
+
+    #     # Number of records in time series group:
+    #     x = utils.select_dict(f2, ts)
+    #     N = len(x)
+
+    #     # Number of unique years in time series group:
+    #     unique_years = utils.unique_dicts(utils.subdict_list(x, ['TIME_PERIOD']))
+    #     n = len(unique_years)
+
+    #     if (N != n):
+    #         has_duplicates.append(ts)
+
+    # if(len(has_duplicates)>0):
+    #     print(f"file {f} has_duplicates")
+
+    #---------------------------------------
+    # Availability calculations
+    #---------------------------------------
+
+    
 
     for ts in unique_TSK_values:
 
-        # Number of records in time series group:
-        x = utils.select_dict(f2, ts)
-        N = len(x)
+        ts_availability = dict(ts)  # or orig.copy()
 
-        # Number of unique years in time series group:
-        unique_years = utils.unique_dicts(utils.subdict_list(x, ['TIME_PERIOD']))
-        n = len(unique_years)
+        # Get list of years (as integers):
 
-        if (N != n):
-            has_duplicates.append(ts)
+        ts_years = utils.subdict_list(utils.select_dict(f2, ts, keep=True), ['TIME_PERIOD'])   
+        years = []
+        for y in ts_years:
+            years.append(int(float(y['TIME_PERIOD'])))
+        # print(years)
+        years.sort()
+    # Get availability stats: min(years), max (years), Number of years
+        ts_availability['years'] = years
+        ts_availability['min_year'] = min(years)
+        ts_availability['max_year'] = max(years)
+        ts_availability['N_years'] = len(years)
 
-    if(len(has_duplicates)>0):
-        print(f"file {f} has_duplicates")
+        availability.append(ts_availability)
+
+utils.dictList2tsv(availability, 'availability_data/ts_series_availability.txt')
+
+
+## Aggregate availability
+
+# 1. How many countries have data (at least 1 data point/year) for each series.
+
+ series = utils.subdict_list(availability, 'MINSET_SERIES')
+
+ for s in series:
+     n_countries = len(unique.select_dict(availability, {'MINSET_SERIES': s['MINSET_SERIES']}, keep=True))
+ 
+ 
+ utils.subdict_list(availability, ['INDICATOR_ID','INDICATOR_DESC','MINSET_SERIES','MINSET_SERIES_DESC'])
+
+
+# 2. How many countries have at least 1 data point since 2015? At least 2 data points since 2015?
+
+
+
+
+
