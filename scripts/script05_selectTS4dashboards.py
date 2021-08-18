de83e3634f81716dfa715c39149794f9c4eeed73
gonzalezmorales <gonzalezmorales@gmail.com>
2021-08-18 16:02:33 -0400

    Selector of time series for dashboards

---
+++ scripts/script05_selectTS4dashboards.py
@@ -0,0 +1,33 @@
+
+import utils
+import utils2
+import json
+from os import listdir
+from os.path import isfile, join
+
+# List of availability_ts files
+datafiles = [f for f in listdir('availability_data/') if isfile(join('availability_data/', f))]
+
+columns_selector = ['INDICATOR_LABEL', 'INDICATOR_ID', 'INDICATOR_DESC', 
+                    'MINSET_SERIES', 'MINSET_SERIES_DESC', 
+                    'TSK_sub_dims', 'TSK_sub_id', 'TSK_sub_desc', 
+                    'sort_order']
+
+ts_catalog = []
+
+
+for fdx, f in enumerate(datafiles):
+
+    if f in ['availability_bySeries.csv','availability_bySeriesCountry.csv']:
+        continue
+
+    # if f != 'availability_ts_100__I_1__I98ba1fd8__S997d1fdb.csv':
+    #     continue
+
+    print(f"\nfile: {f}")
+
+    x = utils.unique_dicts(utils.subdict_list(utils.tsv2dictlist('availability_data/'+ f),columns_selector))
+
+    ts_catalog.extend(x)
+
+utils.dictList2csv(ts_catalog, 'master_data/ts_catalog.csv')
